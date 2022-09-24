009bd39895b62d3a4fe9c4569014a7cb4928c58b
ggyul <wakegyul@gmail.com>
2022-09-25 03:30:16 +0900

    feat: swagger에서 토큰용 헤더 추가한다

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -6,9 +6,12 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -26,6 +29,11 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.example.demo.src"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(swaggerInfo());
+                .apiInfo(swaggerInfo())
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "X-ACCESS-TOKEN", "header");
     }
 }
